ad3ea4d53cda70ff156cf255f33a52c05e7881e5
Tony5897 <tony.martinez5897@gmail.com>
2022-10-24 00:49:46 -0700

    working through logic and debug continuation.

--- stray.c
+++ stray.c
@@ -7,22 +7,25 @@ int main (void)
 
     struct month
     {
-        int numberOfDays;
+        int  numberOfDays;
         char name [3];
     };
-}
 
-const struct month months[12] =
-  { { { 31, {'J', 'a', 'n'}  }, { 28,} {'F', 'e', 'b' } },
-    { { 31, {'M', 'a', 'r'}  }, { 30,} {'A', 'p', 'p' } },
-    { { 31, {'M', 'a', 'y'}  }, { 20,} {'J', 'u', 'n' } },
-    { { 31, {'J', 'u', 'l'}  }, { 31,} {'A', 'u', 'g' } },
-    { { 31, {'S', 'e', 'p'}  }, { 31,} {'O', 'c', 't' } },
-    { { 31, {'N', 'o', 'v'}  }, { 31,} {'D', 'e', 'c' } } };
+    const struct month months[12] =
+      { { { 31, {'J', 'a', 'n'}  }, { 28,} {'F', 'e', 'b' } },
+        { { 31, {'M', 'a', 'r'}  }, { 30,} {'A', 'p', 'p' } },
+        { { 31, {'M', 'a', 'y'}  }, { 20,} {'J', 'u', 'n' } },
+        { { 31, {'J', 'u', 'l'}  }, { 31,} {'A', 'u', 'g' } },
+        { { 30, {'S', 'e', 'p'}  }, { 31,} {'O', 'c', 't' } },
+        { { 30, {'N', 'o', 'v'}  }, { 31,} {'D', 'e', 'c' } } };
 
-printf ("Month  Number of Days\n");
-printf ("-----" ---------------\n");
+    printf ("Month  Number of Days\n");
+    printf ("----- ---------------\n");
 
-return 0;
+    for ( i = 0; i  < 12;   ++1 )
+        printf (" %c%c%c              %i\n",
+            months[i] .name[0], months[i] .name[1],
+            months[i] .name[2], months[i] .numberOfDays);
 
+    return 0;
 }
