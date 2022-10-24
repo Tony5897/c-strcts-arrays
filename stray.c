b7cf24fc7f61b2ad10288bf67b22f35c8499aa55
Tony5897 <tony.martinez5897@gmail.com>
2022-10-24 00:27:52 -0700

    debugging structures

--- stray.c
+++ stray.c
@@ -3,9 +3,26 @@
 int main (void)
 
 {
+    int i;
 
-    printf("Test\n");
+    struct month
+    {
+        int numberOfDays;
+        char name [3];
+    };
+}
 
-    return 0;
+const struct month months[12] =
+  { { { 31, {'J', 'a', 'n'}  }, { 28,} {'F', 'e', 'b' } },
+    { { 31, {'M', 'a', 'r'}  }, { 30,} {'A', 'p', 'p' } },
+    { { 31, {'M', 'a', 'y'}  }, { 20,} {'J', 'u', 'n' } },
+    { { 31, {'J', 'u', 'l'}  }, { 31,} {'A', 'u', 'g' } },
+    { { 31, {'S', 'e', 'p'}  }, { 31,} {'O', 'c', 't' } },
+    { { 31, {'N', 'o', 'v'}  }, { 31,} {'D', 'e', 'c' } } };
+
+printf ("Month  Number of Days\n");
+printf ("-----" ---------------\n");
+
+return 0;
 
 }
